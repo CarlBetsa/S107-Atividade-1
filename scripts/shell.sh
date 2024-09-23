764c0e221ae9634b218da47a7b9e6afc073d8131
Lucas Cinquetti <lucas.cinquetti@ges.inatel.br>
2024-09-23 16:39:19 -0300

    Shell e Workflow

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,2 +1,19 @@
+# Variáveis de ambiente recebidas do GitHub Actions
+RECIPIENT_EMAIL="$RECIPIENT_EMAIL"
+RECIPIENT_EMAIL2="$RECIPIENT_EMAIL2"
+
 sudo apt-get install mailutils
-echo "Mandando e-mail com o mailutils do linux" | mail -s "Pipeline Status" $EMAIL_RECIPIENT
+
+# Verifica se as variáveis necessárias estão presentes
+if [[ -z "$RECIPIENT_EMAIL" && -z "$RECIPIENT_EMAIL2" ]]; then
+  echo "Faltando variáveis de ambiente. Verifique RECIPIENT_EMAIL."
+  exit 1
+fi
+
+# Conteúdo do e-mail
+SUBJECT="Notificação: Execução do Pipeline"
+BODY="Pipeline executado com sucesso!"
+
+# Enviando o e-mail usando o comando sendmail
+echo -e "Subject:${SUBJECT}\n\n${BODY}" | sendmail -v "$RECIPIENT_EMAIL"
+echo -e "Subject:${SUBJECT}\n\n${BODY}" | sendmail -v "$RECIPIENT_EMAIL2"
